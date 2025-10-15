4af4366db51ea8a85c6cb0a698e0ba052911d669
GolfBravoSierra <giovanigiovani.correio@gmail.com>
2025-10-15 15:40:58 -0300

    working apache and msql installation and trying to make json woks

--- webconfig.sh
+++ webconfig.sh
@@ -2,10 +2,14 @@ echo "--> Iniciando configuracao da VM"
 apt-get update
 
 echo "--> Instalando MySQL"
-apt-get install -y mysql
+apt-get install mysql-server -y
 
 echo "--> Instalando Apache"
-apt-get install -y apache
+apt-get install apache2 -y
+
+echo "--> Instalando PHP"
+sudo apt install php-json
+sudo systemctl restart apache2
 
 echo "-->termino da configuracao da VM"
 echo "--> Acesse a aplicacao em http://localhost:8080"
