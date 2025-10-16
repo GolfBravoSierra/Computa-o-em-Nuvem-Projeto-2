abef6f62a0f7f2b576043b72504e48e2a6568b08
GolfBravoSierra <giovanigiovani.correio@gmail.com>
2025-10-16 14:46:21 -0300

    arrumeui a instalação do apache e mysql

--- webconfig.sh
+++ webconfig.sh
@@ -7,9 +7,5 @@ apt-get install mysql-server -y
 echo "--> Instalando Apache"
 apt-get install apache2 -y
 
-echo "--> Instalando PHP"
-sudo apt install php-json
-sudo systemctl restart apache2
-
 echo "-->termino da configuracao da VM"
 echo "--> Acesse a aplicacao em http://localhost:8080"
