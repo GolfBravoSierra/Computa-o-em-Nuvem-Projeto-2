1bd43ca249626e8b0ce0d59591fbff582a9bdfd8
GolfBravoSierra <giovanigiovani.correio@gmail.com>
2025-10-17 09:16:49 -0300

    Pagina HTML do projeto

--- webconfig.sh
+++ webconfig.sh
@@ -7,5 +7,8 @@ apt-get install mysql-server -y
 echo "--> Instalando Apache"
 apt-get install apache2 -y
 
+echo "--> configurando HTML"
+sudo cp /vagrant/index.html /var/www/html/
+
 echo "-->termino da configuracao da VM"
 echo "--> Acesse a aplicacao em http://localhost:8080"
